1910dd6cb3097ac8ea80c951b1195d5677f6c13e
WillowOrmiston <pteropod2005@gmail.com>
2025-03-02 18:30:12 -0600

    initial commit

---
+++ Decode.py
@@ -0,0 +1,91 @@
+
+from lark import Visitor, Lark, Tree, Token
+
+DOWN = '-'
+RIGHT = '/'
+LEFT = '*'
+
+VOWEL = '~'
+CONS = '^'
+
+CALL = '['
+RET = ']'
+# 012345
+# 112345
+# 212345 -- row # and column #
+# set up table? yes ---   
+# a list of list of string
+# a string is text ex- th -- string of characters 0 or more
+table = [
+    ['', '', '', 'H', 'R', '', ''],
+    ['P', 'B', 'M', '', '', '', ''],
+    ['', '', '', 'F', 'V', '', ''],
+    ['', '', '', 'th', 'TH', '', ''],
+    ['T', 'D', 'N', 'S', 'Z', 'L', ''],
+    ['', 'CH', '', 'SH', 'ZH', '', 'J'],
+    ['K', 'G', 'NG', '', '', '', ''],
+]
+# vowel is set up -1 cause its offset from cons 
+vowels = ['OO', 'o', 'Ou', 'AH', 'UH', 'A', 'e', 'I', 'E']
+
+class Output:
+    def __init__(self):
+        self.res = ''
+        # x,y 
+        # 0 indexing,0 is the first item cause its based off of offset 
+        self.pos = (3, 0)
+
+    def add(self, tree):
+        if isinstance(tree, Tree):
+            match tree.data:
+                
+                case 'rec':
+                    # retain position from last turn
+                    last = self.pos
+                    for i in tree.children:
+                        self.add(i)
+                    self.pos = last
+                case 'word':
+                    for i in tree.children:
+                        self.add(i)
+                case 'start':
+                    for i in tree.children:
+                        self.add(i)
+                case _:
+                    print(tree.data)
+        else:
+            match str(tree):
+                # positive in y axis is down?
+                case '-':
+                    self.pos = (self.pos[0], self.pos[1] + 1)
+                case '/':
+                    self.pos = (self.pos[0] + 1, self.pos[1])
+                case '.':
+                    self.pos = (self.pos[0] - 1, self.pos[1])
+                # ok so self.pos[0] is x axis self.pos[1] is y axis
+                # ^ needs both x and y axis 
+                case '^':
+                    self.res += table[self.pos[1]][self.pos[0]]
+                case '~':
+                    self.res += vowels[self.pos[0] + 1]
+                case '[' | ']':
+                    pass
+
+parser = Lark.open('grammar.lark')
+
+def decode(msg):
+    tree = parser.parse(msg)
+    out = Output()
+    out.add(tree)
+    return out.res
+
+def main():
+    #  this is a while loop awawawa
+    while True:
+        msg = input("^-^--> ")
+        res = decode(msg)
+        print(res)
+
+
+if __name__ == '__main__':
+    main()
---
+++ Encode.py
@@ -0,0 +1,31 @@
+
+
+
+# ee-oo # r # h-sh # l # m-p # n-t # ng-k # ch v TH z zh j
+canon = ["[////~]", "[///~]", "[//~]", "[/~]", "[~]", "[.~]", "[..~]", "[...~]", "[....~]", "[/^]", "[^]", "[--^]", "[---^]", "[----^]", "[-----^]", "[//----^]", "[.-^]", "[..-^]", "[...-^]", "[.----^]", "[..----^]", "[...----^]", "[.------^]", "[..------^]", "[...------^]", "[..-----^]", "[/--^]", "[/---^]", "[/----^]", "[/-----^]", "[///----^]"]
+vowcon= ["E", "I", "e", "A", "u", "a", "U", "o", "O"]
+
+# testing purposes get rid of later
+alpha = ["a", "b", "c"]
+
+# def tester:??
+
+
+def encode(message):
+    return canon[int(message)-1]
+
+
+
+location = 0
+location +=
+while location == 0 :
+    movement = input("menu:")
+    print(movement)
+    # location += [int[movement]]
+
+
+while location == 1 :
+    message = input("*-*--> ")
+    result = encode(message)
+    print('output : ' +result)
+
